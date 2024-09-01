cc1ebc05483e8dc284375c9c7c8544fe10d15307
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-09-01 15:53:37 +0300

    UI rating mocking

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Row, Col, Stack, Container } from 'react-bootstrap'
+import { Rating } from 'react-simple-star-rating'
 
 import useLanguage from '../hooks/useLanguage'
 import { convertKeyToSelectedLanguage } from '../i18n/conversion'
@@ -9,13 +10,34 @@ import { convertKeyToSelectedLanguage } from '../i18n/conversion'
 export default function MovieDetails({ movie, genres }) {
   const { i18nData } = useLanguage();
   const navigate = useNavigate();
+  const [rating, setRating] = useState(4);
+
+  const avgRatingMock = 4.89;
+  const nrOfRatingsMock = 5326;
+
+  function handleRating(rate) {
+    setRating(rate);
+  }
 
   return (
     <>
       <h1>{movie.name}</h1>
 
       <Stack direction='horizontal' className='mb-5'>
-        <img className='cover_img_details corner-borders' src={`${process.env.PUBLIC_URL}/covers/${movie.img_name}`} alt={`${movie.title}_cover`} />
+        <Stack direction='vertical' className='me-4'>
+          <img className='cover_img_details corner-borders'
+            src={`${process.env.PUBLIC_URL}/covers/${movie.img_name}`} alt={`${movie.name}_cover`} />
+          <div className='d-flex justify-content-center your-rating mt-1'>
+            <span className='mt-1'>Your rating: </span>
+            <span className='ms-1'>
+              <Rating onClick={handleRating} initialValue={rating} allowFraction transition/>
+            </span>
+          </div>
+          <div className='d-flex justify-content-center watchwise-rating mt-1'>
+            <span >WatchWise rating: </span>
+            <span className='ms-1'>{avgRatingMock}/5 ({nrOfRatingsMock})</span>
+          </div>
+        </Stack>
         <Stack direction='vertical' className='mt-5'>
           <Row className='justify-content-md-center'>
             <Col xs lg={8} className='storyline'>
@@ -25,7 +47,7 @@ export default function MovieDetails({ movie, genres }) {
             </Col>
           </Row>
         </Stack>
-      </Stack>
+      </Stack >
 
       <Container >
         <Row className='ratio ratio-16x9 trailer' >
@@ -47,7 +69,8 @@ export default function MovieDetails({ movie, genres }) {
         </span>
       </Container>
 
-      {genres &&
+      {
+        genres &&
         genres.map((value, indexNr) => {
           return (
             <Container key={`container_${movie._key}_genre_${indexNr}`} >
@@ -67,49 +90,51 @@ export default function MovieDetails({ movie, genres }) {
         })
       }
 
-      {Object.keys(movie).map((key, index) => {
-        if (key === '_key' || key === 'img_name' || key === 'trailer_link'
-          || key === 'name' || key === 'storyline') {
-          return null;
-        }
-        if (Array.isArray(movie[key])) {
-          // if value is an array show elements in different rows
-          return (
-            movie[key].map((value, indexNr) => {
-              return (
-                <Container key={`container_${movie._key}_${index}_${indexNr}`} >
-                  <Row key={`row_${movie._key}_${index}_${indexNr}`} className='mb-1'>
-                    {indexNr === 0 &&
-                      <>
-                        <h4 className='mt-3'>{convertKeyToSelectedLanguage(key, i18nData)}</h4>
-                        <hr className='short-hr'></hr>
-                      </>
-                    }
-                  </Row>
-                  <Row className='big-margin-left'>
-                    {value}
-                  </Row>
-                </Container>
-              )
-            })
-          );
-        }
+      {
+        Object.keys(movie).map((key, index) => {
+          if (key === '_key' || key === 'img_name' || key === 'trailer_link'
+            || key === 'name' || key === 'storyline') {
+            return null;
+          }
+          if (Array.isArray(movie[key])) {
+            // if value is an array show elements in different rows
+            return (
+              movie[key].map((value, indexNr) => {
+                return (
+                  <Container key={`container_${movie._key}_${index}_${indexNr}`} >
+                    <Row key={`row_${movie._key}_${index}_${indexNr}`} className='mb-1'>
+                      {indexNr === 0 &&
+                        <>
+                          <h4 className='mt-3'>{convertKeyToSelectedLanguage(key, i18nData)}</h4>
+                          <hr className='short-hr'></hr>
+                        </>
+                      }
+                    </Row>
+                    <Row className='big-margin-left'>
+                      {value}
+                    </Row>
+                  </Container>
+                )
+              })
+            );
+          }
 
-        return (
-          <Container key={`container_${movie._key}_${index}`} >
-            <Row key={`${movie._key}_${index}`} className='mb-2'>
-              <h4 className='mt-3'>{convertKeyToSelectedLanguage(key, i18nData)}</h4>
-              <hr className='short-hr'></hr>
-            </Row>
-            <Row>
-              {key === 'imdb_link' ?
-                <a href={movie[key]} className='big-margin-left mb-5'>{movie[key]}</a>
-                :
-                <span className='big-margin-left'>{movie[key]}</span>}
-            </Row>
-          </Container>
-        );
-      })}
+          return (
+            <Container key={`container_${movie._key}_${index}`} >
+              <Row key={`${movie._key}_${index}`} className='mb-2'>
+                <h4 className='mt-3'>{convertKeyToSelectedLanguage(key, i18nData)}</h4>
+                <hr className='short-hr'></hr>
+              </Row>
+              <Row>
+                {key === 'imdb_link' ?
+                  <a href={movie[key]} className='big-margin-left mb-5'>{movie[key]}</a>
+                  :
+                  <span className='big-margin-left'>{movie[key]}</span>}
+              </Row>
+            </Container>
+          );
+        })
+      }
     </>
   )
 }
