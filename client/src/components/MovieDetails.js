227eeb8dfa0f5870b718e1f657e04f29e204a508
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-09-01 18:35:23 +0300

    ui rating i18n and logged in changes

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -4,12 +4,14 @@ import { Row, Col, Stack, Container } from 'react-bootstrap'
 import { Rating } from 'react-simple-star-rating'
 
 import useLanguage from '../hooks/useLanguage'
-import { convertKeyToSelectedLanguage } from '../i18n/conversion'
+import { convertKeyToSelectedLanguage, convertBasedOnRatingsToLanguage } from '../i18n/conversion'
+import useAuth from '../hooks/useAuth'
 
 
 export default function MovieDetails({ movie, genres }) {
-  const { i18nData } = useLanguage();
+  const { i18nData, language } = useLanguage();
   const navigate = useNavigate();
+  const { auth } = useAuth();
   const [rating, setRating] = useState(4);
 
   const avgRatingMock = 4.89;
@@ -27,15 +29,20 @@ export default function MovieDetails({ movie, genres }) {
         <Stack direction='vertical' className='me-4'>
           <img className='cover_img_details corner-borders'
             src={`${process.env.PUBLIC_URL}/covers/${movie.img_name}`} alt={`${movie.name}_cover`} />
-          <div className='d-flex justify-content-center your-rating mt-1'>
-            <span className='mt-1'>Your rating: </span>
-            <span className='ms-1'>
-              <Rating onClick={handleRating} initialValue={rating} allowFraction transition/>
-            </span>
+          {auth.logged_in &&
+            <div className='d-flex justify-content-center your-rating mt-1'>
+              <span className='mt-1 fw-bold'>{convertKeyToSelectedLanguage('your_rating', i18nData)}: </span>
+              <span className='ms-1'>
+                <Rating onClick={handleRating} initialValue={rating} allowFraction transition />
+              </span>
+            </div>
+          }
+          <div className='d-flex justify-content-center watchwise-rating mt-2'>
+            <span className='fw-bold'>{convertKeyToSelectedLanguage('ww_rating', i18nData)}: </span>
+            <span className='ms-2'>{avgRatingMock} / 5</span>
           </div>
-          <div className='d-flex justify-content-center watchwise-rating mt-1'>
-            <span >WatchWise rating: </span>
-            <span className='ms-1'>{avgRatingMock}/5 ({nrOfRatingsMock})</span>
+          <div className='d-flex justify-content-center watchwise-rating'>
+            <span>{convertBasedOnRatingsToLanguage(language, nrOfRatingsMock, i18nData)}</span>
           </div>
         </Stack>
         <Stack direction='vertical' className='mt-5'>
