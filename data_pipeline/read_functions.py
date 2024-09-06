02fc193adffb9d3d5b5e176ccab2de8fb5af013d
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-09-06 18:40:20 +0300

    read users, ratings and genres

--- data_pipeline/read_functions.py
+++ data_pipeline/read_functions.py
@@ -1,16 +1,17 @@
 import csv
-import random
+from datetime import datetime, timezone
 
 movies_file_path = './data/movies.csv'
 links_file_path = './data/movie_links.csv'
 tags_file_path = './data/movie_tags.csv'
+ratings_file_path = './data/movie_ratings.csv'
 
-
-def read_movies(file_path):
+def read_movies_and_genres(file_path):
     with open(file_path, mode='r', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)
         movies = []
+        genres = set()
         for row in csv_reader:
             # only keep movies with genres
             if row[2] == '(no genres listed)':
@@ -20,8 +21,8 @@ def read_movies(file_path):
                 'title': row[1],
                 'genres': row[2].split('|') if row[2] else []
             })
-        
-        return movies
+            genres.update(row[2].split('|'))
+        return movies, genres
 
 
 def read_links(file_path):
@@ -44,9 +45,9 @@ def read_tags(file_path):
         return tags
 
 
-def get_movies_with_links():
+def get_movies_with_links_and_genres():
     # merge movies with links
-    movies = read_movies(movies_file_path)
+    movies, genres = read_movies_and_genres(movies_file_path)
     links = read_links(links_file_path)
 
     movies_with_links = []
@@ -56,5 +57,23 @@ def get_movies_with_links():
             movie['imdb_link'] = links[movie_id]
         movies_with_links.append(movie)
 
-    return movies_with_links
+    return movies_with_links, genres
+
+
+def read_ratings_and_users(file_path):
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        ratings = []
+        users = set()
+        for row in csv_reader:
+            timestamp = datetime.fromtimestamp(int(row[3]), tz=timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+            ratings.append({
+                'userId': row[0],
+                'movieId': row[1],
+                'rating': float(row[2]),
+                'timestamp': timestamp
+            })
+            users.add(row[0])
+        return ratings, users
 
