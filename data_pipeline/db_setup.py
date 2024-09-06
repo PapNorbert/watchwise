daab96d76a81562f1609b88f57e15b4fd77e9314
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-09-06 17:18:07 +0300

    connection to db and collection creation

---
+++ data_pipeline/db_setup.py
@@ -0,0 +1,40 @@
+from db_connection import get_db
+
+
+def initialize_collections():
+    try:
+        db = get_db()
+        collections = [
+            'movies', 'series', 'users', 'watch_groups',
+            'genres', 'opinion_threads', 'moderator_requests',
+            'watch_group_chats', 'tags', 'announcements'
+        ]
+        edge_collections = [
+            'joined_group',
+            'his_group_chat',
+            'join_request',
+            'follows_thread',
+            'his_type',
+            'is_about_show',
+            'has_rated'
+        ]
+        for collection_name in collections:
+            try:
+                if not db.has_collection(collection_name):
+                    db.create_collection(collection_name)
+            except Exception as e:
+                print(f"Error creating collection {collection_name}: {e}")
+                
+        for edge_collection_name in edge_collections:
+            try:
+                if not db.has_collection(edge_collection_name):
+                    db.create_collection(edge_collection_name, edge=True)
+            except Exception as e:
+                print(f"Error creating edge collection {edge_collection_name}: {e}")
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+initialize_collections()
