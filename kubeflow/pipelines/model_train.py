07741f76ad32b4453273fda817d7a7c91ee42197
Pap Norbert <116946410+PapNorbert@users.noreply.github.com>
2025-04-23 18:53:29 +0300

    embeddings update after training db part

--- kubeflow/pipelines/model_train.py
+++ kubeflow/pipelines/model_train.py
@@ -35,20 +35,25 @@ def get_train_data(minio_endpoint: str, minio_access_key: str, minio_secret_key:
 
 
 @dsl.component(packages_to_install=['transformers==4.45.2', 'sentence-transformers==3.1.1', 
-    'boto3==1.36.16', 'datasets==3.1.0', 'torch==2.5.0', 'accelerate==1.1.1'])
+    'boto3==1.36.16', 'datasets==3.1.0', 'torch==2.5.0', 'accelerate==1.1.1', 'python-arango==8.1.0'])
 def train_model(
         minio_endpoint: str, minio_access_key: str, minio_secret_key: str,
-        model_name: str,
+        url: str, db_name: str, username: str, password: str,
+        model_name: str, fields_to_use: list,
         train_data_folder: dsl.InputPath()
     ):
     import os
     import json
     import boto3
     import zipfile
+    import ast
+    import csv
+    import uuid
     from datasets import Dataset
     from sentence_transformers import SentenceTransformer, losses, SentenceTransformerTrainingArguments, \
         SentenceTransformerTrainer
     from sklearn.model_selection import train_test_split
+    from arango import ArangoClient
 
 
     MODEL_BUCKET = "models"
@@ -129,6 +134,172 @@ def train_model(
                     zipf.write(abs_path, os.path.join(model_name, rel_path))
         print(f"ZIP created at {output_zip}.zip")
 
+    def update_embeddings(model, fields_to_use):
+
+        def clear_collection(collection_name):
+            try:
+                client = ArangoClient(hosts=url, request_timeout=240, verify_override=False)
+                db = client.db(db_name, username=username, password=password)
+                collection = db.collection(collection_name)
+                collection.truncate()
+                print(f"Collection '{collection_name}' has been cleared.")
+                return True
+            except Exception as e:
+                print(f"Error clearing collection '{collection_name}': {e}")
+                return False
+
+        def read_movie_embeddings(file_path):
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                csv_reader = csv.reader(file)
+                header = next(csv_reader)
+                movies_w_embeddings = []
+                for row in csv_reader:
+                    row_embedding = ast.literal_eval(row[14]) if row[14] else []
+                    movies_w_embeddings.append({
+                        'movieId': row[0],
+                        'name': row[4],
+                        'poster': row[12],
+                        'embedding': row_embedding,
+                    })
+                return movies_w_embeddings
+
+        def read_serie_embeddings(file_path):
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                csv_reader = csv.reader(file)
+                _header = next(csv_reader)
+                movies_w_embeddings = []
+                for row in csv_reader:
+                    row_embedding = ast.literal_eval(row[18]) if row[18] else []
+                    movies_w_embeddings.append({
+                        'series_id': row[0],
+                        'name': row[3],
+                        'poster': row[14],
+                        'embedding': row_embedding,
+                    })
+                return movies_w_embeddings
+
+        def generate_embeddings_sentence_transformer(shows, fields_to_use, model):
+            show_texts = []
+            for show in shows:
+                combined_text = []
+                for field in fields_to_use:
+                    if isinstance(show[field], list):
+                        combined_text.append(', '.join(show[field]))
+                    else:
+                        combined_text.append(show[field])
+                show_texts.append(' '.join(combined_text))
+            embeddings = model.encode(show_texts, show_progress_bar=True)
+            for i, show in enumerate(shows):
+                show['embedding'] = embeddings[i].tolist()
+            return shows
+    
+        def save_embeddings_to_database(movie_embeddings, series_embeddings):
+            try:
+                embeddings = []
+                has_embedding_edges = []
+                for movie_embedding in movie_embeddings:
+                    embedding_key = generate_key()
+                    movie_key = movie_embedding['movieId']
+                    embeddings.append({
+                        '_key': embedding_key,
+                        'show_key': movie_key,
+                        'show_type': 'movie',
+                        'show_name': movie_embedding['name'],
+                        **({'img_name': movie_embedding['poster']} if
+                        'poster' in movie_embedding and movie_embedding['poster'] != 'N/A' else {}),
+                        'embedding_vector': movie_embedding['embedding']
+                    })
+                    has_embedding_edges.append({
+                        '_key': embedding_key,
+                        '_from': f"movies/{movie_key}",
+                        '_to': f"embeddings/{embedding_key}"
+                    })
+                for serie_embeddings in series_embeddings:
+                    embedding_key = generate_key()
+                    serie_key = serie_embeddings['series_id']
+                    embeddings.append({
+                        '_key': embedding_key,
+                        'show_key': serie_key,
+                        'show_type': 'serie',
+                        'show_name': serie_embeddings['name'],
+                        **({'img_name': serie_embeddings['poster']} if
+                        'poster' in serie_embeddings and serie_embeddings['poster'] != 'N/A' else {}),
+                        'embedding_vector': serie_embeddings['embedding']
+                    })
+                    has_embedding_edges.append({
+                        '_key': embedding_key,
+                        '_from': f"series/{serie_key}",
+                        '_to': f"embeddings/{embedding_key}"
+                    })
+
+                print('Saving', len(embeddings), 'embeddings')
+                save_many_to_database_in_batches('embeddings', embeddings)
+                print('Saving', len(has_embedding_edges), 'has embedding edges')
+                save_many_to_database_in_batches('has_embedding', has_embedding_edges)
+            except Exception as e:
+                print(e)
+
+        def generate_key() -> str:
+            try:
+                unique_id = uuid.uuid4()
+                numeric_key = unique_id.int
+                return str(numeric_key)
+            except Exception as e:
+                print(e)
+
+        def save_many_to_database_in_batches(collection_name, data, batch_size=1000):
+            try:
+                client = ArangoClient(hosts=url, request_timeout=2000, verify_override=False)
+                db = client.db(db_name, username=username, password=password)
+                collection = db.collection(collection_name)
+                # Break the data into smaller batches and insert them
+                for i in range(0, len(data), batch_size):
+                    batch = data[i:i+batch_size]
+                    result = collection.insert_many(batch, overwrite=True, overwrite_mode='update')
+                    print(f'Saved batch of {len(batch)} items to {collection_name}')
+                
+                return result
+            except Exception as e:
+                print(f"Error saving {collection_name}: {e}")
+                return []
+        
+
+        DATA_BUCKET = "data"
+
+        movie_embeddings = []
+        serie_embeddings = []
+
+        clear_collection('embeddings')
+
+        # download show files
+        shows_dir = '/tmp/shows'
+        os.makedirs(shows_dir, exist_ok=True)
+        prefixes = ["embeddings/initial", "embeddings/new"]
+        for prefix in prefixes:
+            objects = s3_client.list_objects_v2(Bucket=DATA_BUCKET, Prefix=prefix)
+            if "Contents" in objects:
+                for obj in objects.get("Contents", []):
+                    file_key = obj["Key"]
+                    local_file_path = os.path.join(shows_dir, os.path.basename(file_key))
+                    s3_client.download_file(DATA_BUCKET, file_key, local_file_path)
+                    print(f"Downloaded: {file_key} → {local_file_path}")
+                    # processing the file
+                    if 'movie' in file_key:
+                        movies_w_embeddings = read_movie_embeddings(file)
+                        current_movies_embedding = generate_embeddings_sentence_transformer(movies_w_embeddings, fields_to_use, model)
+                        movie_embeddings.append(current_movies_embedding)
+                        save_embeddings_to_database(current_movies_embedding, [])
+                    else:
+                        # file contains series
+                        series_w_embeddings = read_serie_embeddings(file)
+                        current_series_embedding = generate_embeddings_sentence_transformer(series_w_embeddings, fields_to_use, model)
+                        serie_embeddings.append(current_series_embedding)
+                        save_embeddings_to_database([], current_series_embedding)
+
+
+        print("Updating all embeddings.")
+        return
+
     os.makedirs(TEMP_DIR, exist_ok=True)
     s3_client.download_file(MODEL_BUCKET, f"{model_name}.zip", MODEL_ZIP_PATH)
     print(f"Downloaded {model_name}.zip to {MODEL_ZIP_PATH}")
@@ -158,6 +329,7 @@ def train_model(
     trained_model_path = "/tmp/model/trained/final"
     fine_tuned_model = fine_tune_model(model, train_data, trained_model_path, 3)
     print(f"Training done.")
+    update_embeddings(fine_tuned_model, fields_to_use)
     zip_path = f"/tmp/{model_name}.zip"
     zip_folder(trained_model_path, zip_path, model_name)
     s3_client.upload_file(f"{zip_path}", MODEL_BUCKET, f"{model_name}.zip")
@@ -210,9 +382,16 @@ def data_processing_pipeline():
     )
     
     model_name='watchwise-20-ep'
+    fields_to_use = ['name', 'plot', 'genres', 'directors', 'actors']
+    url = 'https://arangodb.default.svc.cluster.local:8529'
+    password=''
+    username='root'
+    db_name='watchwiseRecommend'
     train_model_task = train_model(
         minio_endpoint=minio_endpoint, minio_access_key=access_key, minio_secret_key=secret_key,
+        url=url, db_name=db_name, username=username, password=password,
         model_name=model_name,
+        fields_to_use=fields_to_use,
         train_data_folder=get_train_data_task.output
     ).set_memory_limit("6Gi").set_cpu_limit("4")
 
